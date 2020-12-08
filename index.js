3c50683e96699d0efbf01aa42f2598000e71c004
Reza Andriansyah <technoboth@gmail.com>
2020-12-09 00:33:56 +0700

    add multiple core in microservice

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const { PORT } = require('./lib').env;
 
-const fastify = require('fastify')();
+const numCPU  = require('os').cpus().length;
+const fastify  = require('fastify')();
+const cluster = require('cluster');
 
-fastify.register(require('fastify-cors'));
-
-fastify.register(require('./router/Find'), {prefix:'/api/find'});
-fastify.register(require('./router/Insert'), {prefix:'/api/insert'});
-fastify.register(require('./router/Update'), {prefix:'/api/update'});
-fastify.register(require('./router/Delete'), {prefix:'/api/delete'});
-
-
-fastify.listen(PORT, function (err) {
-  if (err) {console.log(err);process.exit(1)}
-  console.log(`This service running in PORT ${PORT}`);
-});
+if(cluster.isMaster){
+  for (let i = 0; i < numCPU; i++) {
+    cluster.fork();
+  }
+}else{
+  fastify.register(require('fastify-cors'));
+  fastify.register(require('./router/Find'), {prefix:'/api/find'});
+  fastify.register(require('./router/Insert'), {prefix:'/api/insert'});
+  fastify.register(require('./router/Update'), {prefix:'/api/update'});
+  fastify.register(require('./router/Delete'), {prefix:'/api/delete'});
+  
+  fastify.listen(PORT, function (err) {
+    if (err) {console.log(err);process.exit(1)}
+    console.log(`This service running in PORT ${PORT}`);
+  });
+}
