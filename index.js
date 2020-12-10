2b4e89ce4e25d32ee8bb92ffc2521d5335d52401
Reza Andriansyah <technoboth@gmail.com>
2020-12-10 10:02:00 +0700

    open listening on all ip

--- index.js
+++ index.js
@@ -4,39 +4,24 @@ const numCPU  = require('os').cpus().length;
 const fastify = require('fastify')();
 const cluster = require('cluster');
 
-fastify.get('/', async ()=>{
-  return {message:"Hello"};
-});
+if(cluster.isMaster){
+  for (let i = 0; i < numCPU; i++) {
+    cluster.fork();
+  }
+}else{
 
-fastify.register(require('fastify-cors'));
-fastify.register(require('./router/Find'), {prefix:'/api/find'});
-fastify.register(require('./router/Insert'), {prefix:'/api/insert'});
-fastify.register(require('./router/Update'), {prefix:'/api/update'});
-fastify.register(require('./router/Delete'), {prefix:'/api/delete'});
+  fastify.get('/', async (req, res)=>{
+    return {message:"Hello"};
+  });
 
-fastify.listen(PORT, function (err) {
-  if (err) {console.log(err);process.exit(1)}
-  console.log(`This service running in PORT ${PORT}`);
-});
-
-// if(cluster.isMaster){
-//   for (let i = 0; i < numCPU; i++) {
-//     cluster.fork();
-//   }
-// }else{
-
-//   fastify.get('/', async (req, res)=>{
-//     return {message:"Hello"};
-//   });
-
-//   fastify.register(require('fastify-cors'));
-//   fastify.register(require('./router/Find'), {prefix:'/api/find'});
-//   fastify.register(require('./router/Insert'), {prefix:'/api/insert'});
-//   fastify.register(require('./router/Update'), {prefix:'/api/update'});
-//   fastify.register(require('./router/Delete'), {prefix:'/api/delete'});
+  fastify.register(require('fastify-cors'));
+  fastify.register(require('./router/Find'), {prefix:'/api/find'});
+  fastify.register(require('./router/Insert'), {prefix:'/api/insert'});
+  fastify.register(require('./router/Update'), {prefix:'/api/update'});
+  fastify.register(require('./router/Delete'), {prefix:'/api/delete'});
   
-//   fastify.listen(PORT, function (err) {
-//     if (err) {console.log(err);process.exit(1)}
-//     console.log(`This service running in PORT ${PORT}`);
-//   });
-// }
+  fastify.listen(PORT, '0.0.0.0' ,function (err) {
+    if (err) {console.log(err);process.exit(1)}
+    console.log(`This service running in PORT ${PORT}`);
+  });
+}
